26f5ddedfa5aaa0fe4d6d5811d7e0ec60ede1439
Avinash <avinashkumarofficial95@gmail.com>
2024-12-20 18:47:18 +0530

    streaming examples

--- streaming/backend/api.jsx
+++ streaming/backend/api.jsx
@@ -4,13 +4,44 @@ const fs = require('fs');
 const cors = require('cors');
 app.use(cors());
 
-app.get('/memoryIntensive', function(req,res){
-    console.log('reading the content');
-    const fileContent = fs.readFileSync('wa-clone-main.zip');
-    console.log('content read finish');
-    res.send(fileContent);
+// app.get('/memoryIntensive', function(req,res){
+//     try{
+//         console.log('reading the content');
+//         const fileContent = fs.readFileSync('Jio-clone-main.zip');
+//         console.log('content reading finish');
+//         res.send(fileContent);
+//     }catch(err){
+//         console.log('err', err);
+//         response.status(500).json({
+//             status: 'failure',
+//             message: err.message
+//         })
+//     }
+   
+// })
+
+app.get("/streamfile", function (req, res) {
+    try{
+        console.log("file readStream created");
+        // const fileStreamInstance = fs.createReadStream("chat-app.zip");
+        const videoStreamInstance = fs.createReadStream("video.mp4");
+        // request , response -> streams 
+        // request -> readble stream 
+        // response -> writable stream
+        res.writeHead(200, {
+            "Content-Type": "video/mp4"
+        })
+        videoStreamInstance.pipe(res);
+    }catch(err){
+        console.log('err', err);
+        response.status(500).json({
+            status: 'failure',
+            message: err.message
+        })
+    }
+    
 })
 
-app.listen(3000,function(){
-    console.log('Server running at port 3000');
+app.listen(3000, function () {
+    console.log("server is runnig at port 3000");
 })
